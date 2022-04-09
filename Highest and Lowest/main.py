70afa280cc352d04cbc0d1af19f54edfe54d2299
fclaudius <65337980+fclaudius@users.noreply.github.com>
2022-04-09 22:29:50 +0200

    2nd Version of Highest and Lowest

--- Highest and Lowest/main.py
+++ Highest and Lowest/main.py
@@ -2,9 +2,4 @@ def high_and_low(numbers):
     lst_nums = []
     for num in numbers.split(" "):
         lst_nums.append(int(num))
-
-    lst_nums.sort()
-    min_num = lst_nums[0]
-    max_num = lst_nums[len(lst_nums)-1]
-    return f"{max_num} {min_num}"
-
+    return f"{max(lst_nums)} {min(lst_nums)}"
