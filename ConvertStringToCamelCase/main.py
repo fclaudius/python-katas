863a81c9b98ceeb857620b3154448a22d0e504f3
fclaudius <65337980+fclaudius@users.noreply.github.com>
2022-04-10 22:31:55 +0200

    Add ConvertStringToCamelCase

---
+++ ConvertStringToCamelCase/main.py
@@ -0,0 +1,31 @@
+import re
+
+
+def to_camel_case(text):
+    reg1 = re.compile("(_|-)[a-zA-Z]")
+    reg2 = re.compile("[a-zA-Z]")
+    while re.search(r"_|-", text) is not None:
+        text = text.replace( myfirst(text, reg1),  replacement(myfirst(text, reg1), reg2))
+
+    return text
+
+
+def myfirst(text, reg1):
+    my = re.search(reg1, text)
+    if my is None: my = ""
+    else:
+        my = str(my.group())
+    return my
+
+
+def replacement(atext, reg2):
+    my = re.search(reg2, atext)
+    if my is None:
+        my = ""
+    else:
+        my = str(my.group()).upper()
+    return my
+
+
+print(to_camel_case("the_stealth_warrior"))
+print(to_camel_case("The-Stealth-Warrior"))
---
+++ ConvertStringToCamelCase/sampe_tests.py
@@ -0,0 +1,9 @@
+import codewars_test as test
+from main import to_camel_case
+
+test.describe("Testing function to_camel_case")
+test.it("Basic tests")
+test.assert_equals(to_camel_case(''), '', "An empty string was provided but not returned")
+test.assert_equals(to_camel_case("the_stealth_warrior"), "theStealthWarrior", "to_camel_case('the_stealth_warrior') did not return correct value")
+test.assert_equals(to_camel_case("The-Stealth-Warrior"), "TheStealthWarrior", "to_camel_case('The-Stealth-Warrior') did not return correct value")
+test.assert_equals(to_camel_case("A-B-C"), "ABC", "to_camel_case('A-B-C') did not return correct value")
