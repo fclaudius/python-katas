4e0153d17c0c42e24466e433b8607e9aad7cba3c
fclaudius <65337980+fclaudius@users.noreply.github.com>
2022-04-10 17:52:05 +0200

    Add longest, better version

--- Two to One/main.py
+++ Two to One/main.py
@@ -1,14 +1,2 @@
 def longest(a1, a2):
-    unique_string = dict()
-    for element in a1 + a2:
-        unique_string[element] = None
-    unique_string = sorted(unique_string)
-    neu = str()
-    for element in unique_string:
-        neu += element
-    print(neu)
-    return neu
-
-
-longest("aretheyhere","yestheyarehere")
-# "aehrsty"
+    return "".join(sorted(set(a1+a2)))
